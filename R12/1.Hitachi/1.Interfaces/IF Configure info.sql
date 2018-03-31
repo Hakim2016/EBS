7a791129cd153cddeb94813af44ee12bad64094e
何晶晶 / He，Jing Jing <jingjing.he@itg.hitachi.cn>
2018-03-31 16:16:35 +0800

    Config

---
+++ R12/1.Hitachi/1.Interfaces/IF Configure info.sql
@@ -0,0 +1,10 @@
+SELECT xic.interface_code,
+       xic.user_concurrent_program_name,
+       decode(xic.type, 'I', 'Inbound', 'O', 'Outbound') type,
+       xic.object_owner,
+       xic.remote_system_code,
+       XIC.*
+  FROM apps.xxfnd_interface_config_v xic
+ WHERE 1 = 1
+  --AND xic.remote_system_code = 'R3'
+ ORDER BY 1
